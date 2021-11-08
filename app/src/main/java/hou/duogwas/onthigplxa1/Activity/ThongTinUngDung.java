1adff564e8b790256c177cded5035b4a647bdc79
duogwas <duong2001n@gmail.com>
2021-11-08 17:00:25 +0700

    add ads kit vao ThongTinUngDung

--- app/src/main/java/hou/duogwas/onthigplxa1/Activity/ThongTinUngDung.java
+++ app/src/main/java/hou/duogwas/onthigplxa1/Activity/ThongTinUngDung.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.BannerAdSize;
+import com.huawei.hms.ads.HwAds;
+import com.huawei.hms.ads.banner.BannerView;
 
 import hou.duogwas.onthigplxa1.R;
 
@@ -24,5 +30,22 @@ public class ThongTinUngDung extends AppCompatActivity {
                 startActivity(new Intent(ThongTinUngDung.this,MainActivity.class));
             }
         });
+
+        // Initialize the HUAWEI Ads SDK.
+        HwAds.init(this);
+
+        //Obatian BannerView from layout activity_main.xml
+        BannerView bottomBannerView = findViewById(R.id.hw_banner_view);
+        AdParam adParam = new AdParam.Builder().build();
+        bottomBannerView.loadAd(adParam);
+
+        //Call new BannerView(Context ThongTinUngDung) to create a BannerView class
+        BannerView topBannerView = new BannerView(this);
+        topBannerView.setAdId("testw6vs28auh3");
+        topBannerView.setBannerAdSize(BannerAdSize.BANNER_SIZE_360_57);
+        topBannerView.loadAd(adParam);
+
+        RelativeLayout rootView = findViewById(R.id.root_view);
+        rootView.addView(topBannerView);
     }
 }
